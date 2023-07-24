e3a35eb93731132ae91393f9d09c31255e08c7e8
TULASIDAS_REDDY <tulasidasreddy0616@gmail.com>
2023-07-24 12:45:06 +0530

    changes for code das_0616

--- ssdblog/src/main/java/com/ssd/blog/web/PostRestController.java
+++ ssdblog/src/main/java/com/ssd/blog/web/PostRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +26,10 @@ public class PostRestController {
 		
 		return new ResponseEntity<PostDto> ( postServiceImpl.sendPost(postDto),HttpStatus.CREATED);
 	}
-
+	@GetMapping("/get")
+	 public String display()
+	 {
+		return "it's ready";
+		 
+	 }
 }
