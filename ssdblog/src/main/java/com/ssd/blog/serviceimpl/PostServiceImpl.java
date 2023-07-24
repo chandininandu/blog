90df62b8b4b93d9837fd4cf7852bacb2771c3348
chandininandu <139625683+chandininandu@users.noreply.github.com>
2023-07-24 11:28:21 +0530

    added for new project

---
+++ ssdblog/src/main/java/com/ssd/blog/serviceimpl/PostServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ssd.blog.serviceimpl;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssd.blog.entity.Post;
+import com.ssd.blog.payload.PostDto;
+import com.ssd.blog.repository.PostRepository;
+
+@Service
+public class PostServiceImpl {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public PostDto sendPost(PostDto dto) {
+		
+		Post post = this.modelMapper.map(dto, Post.class);
+		post = postRepository.save(post);
+		dto = this.modelMapper.map(post, PostDto.class);
+		
+		return dto;
+	}
+
+}
