b81e7f7bd2b7b0092ed29a0ebf5bc4d81c97a80c
LokeshAdapka <premamlokesh@gmail.com>
2023-07-24 16:19:04 +0530

    code changes for loki01

--- ssdblog/src/main/java/com/ssd/blog/serviceimpl/PostServiceImpl.java
+++ ssdblog/src/main/java/com/ssd/blog/serviceimpl/PostServiceImpl.java
@@ -1,5 +1,8 @@
 package com.ssd.blog.serviceimpl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,5 +28,20 @@ public class PostServiceImpl {
 		
 		return dto;
 	}
-
+	
+	public PostDto getPostById(Long id) {
+		Post post=postRepository.findById(id).get();
+		PostDto pDto=modelMapper.map(post, PostDto.class);
+		return pDto;
+	}
+	
+	public List<PostDto> getAllPosts(){
+		List<Post> posts=postRepository.findAll();
+		return posts.stream().map((Post)->modelMapper.map(Post, PostDto.class)).collect(Collectors.toList());
+	}
+	
+	public String postDelateById(Long id) {
+		postRepository.deleteById(id);
+		return "record deleted ";
+		}
 }
--- ssdblog/src/main/java/com/ssd/blog/web/PostRestController.java
+++ ssdblog/src/main/java/com/ssd/blog/web/PostRestController.java
@@ -1,14 +1,18 @@
 package com.ssd.blog.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.ssd.blog.payload.PostDto;
 import com.ssd.blog.serviceimpl.PostServiceImpl;
@@ -23,7 +27,22 @@ public class PostRestController {
 	@PostMapping("/posts")
 	public ResponseEntity<PostDto> sendPost(@Validated  @RequestBody PostDto postDto){
 		
-		return new ResponseEntity<PostDto> ( postServiceImpl.sendPost(postDto),HttpStatus.CREATED);
+	return new ResponseEntity<PostDto> ( postServiceImpl.sendPost(postDto),HttpStatus.CREATED);
 	}
-
+	
+	@GetMapping("/posts/{id}")
+	public ResponseEntity<PostDto> getPostById(@PathVariable("id") Long id){
+		PostDto post=postServiceImpl.getPostById(id);
+		return new ResponseEntity<PostDto>(post,HttpStatus.OK);
+	}
+	@GetMapping("/posts")
+	public ResponseEntity<List<PostDto>> getAllPosts(){
+		List<PostDto> dto=postServiceImpl.getAllPosts();
+		return new ResponseEntity<List<PostDto>>(dto,HttpStatus.OK);
+	}
+	@DeleteMapping("/posts/{id}")
+	public ResponseEntity<String> deleteById(@PathVariable("id") Long id){
+		String msg=postServiceImpl.postDelateById(id);
+		return new ResponseEntity<String>("record is deleted",HttpStatus.OK);
+	}
 }
