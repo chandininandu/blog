89aca514bbddc0a2271577be5eaa62bf6c590493
chowdary-1947 <120239068+chowdary-1947@users.noreply.github.com>
2023-08-01 16:50:09 +0530

    code for email sending through spring boot

---
+++ email/src/main/java/com/ssd/email/EmailApplication.java
@@ -0,0 +1,26 @@
+package com.ssd.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class EmailApplication {
+
+	@Autowired
+	public EmailService emailService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EmailApplication.class, args);
+		
+	
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void mailmessage() {
+		emailService.sendMail("@gmail.com", "", "");
+		
+	}
+}
