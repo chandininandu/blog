8f0833668a0f5104b457a6b7b95b021609f76b1f
pintu70 <maharana.pintu@gmail.com>
2023-08-02 17:00:16 +0530

    code created for comments many to one

---
+++ ssdblog/src/main/java/com/ssd/blog/payload/CommentDto.java
@@ -0,0 +1,92 @@
+package com.ssd.blog.payload;
+
+
+	
+
+import lombok.Data;
+
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+@Data
+public class CommentDto {
+    private long id;
+    // name should not be null or empty
+    @NotEmpty(message = "Name should not be null or empty")
+    private String name;
+
+    // email should not be null or empty
+    // email field validation
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
+    private String email;
+
+    // comment body should not be bull or empty
+    // Comment body must be minimum 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Comment body must be minimum 10 characters")
+    private String body;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public CommentDto(long id, @NotEmpty(message = "Name should not be null or empty") String name,
+			@NotEmpty(message = "Email should not be null or empty") @Email String email,
+			@NotEmpty @Size(min = 10, message = "Comment body must be minimum 10 characters") String body) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.body = body;
+	}
+
+	public CommentDto(@NotEmpty(message = "Name should not be null or empty") String name,
+			@NotEmpty(message = "Email should not be null or empty") @Email String email,
+			@NotEmpty @Size(min = 10, message = "Comment body must be minimum 10 characters") String body) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.body = body;
+	}
+
+	public CommentDto() {
+		super();
+	}
+    
+    
+}
+
+
+
